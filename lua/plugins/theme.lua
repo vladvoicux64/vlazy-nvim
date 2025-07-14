406514cd16dce0631e41a68a7f4219b8c8cfbdb0
Voicu Ioan-Vladut <77429938+vladvoicux64@users.noreply.github.com>
2025-07-14 13:07:56 +0300

    Updated theme.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,63 @@
 return {
-  {
-    "oneslash/helix-nvim",
-    lazy = false, -- load immediately
-    priority = 1000, -- load before others
+  "catppuccin/nvim",
+  lazy = true,
+  name = "catppuccin",
+  opts = {
+    integrations = {
+      aerial = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      flash = true,
+      flavour = "macchiato",
+      fzf = true,
+      grug_far = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      markdown = true,
+      mini = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      semantic_tokens = true,
+      snacks = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
   },
-
+  specs = {
+    {
+      "akinsho/bufferline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if (vim.g.colors_name or ""):find("catppuccin") then
+          opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+        end
+      end,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "helix",
+      colorscheme = "catppuccin-macchiato",
     },
-    dependencies = { "oneslash/helix-nvim" }, -- ensure helix-nvim loads first
   },
 }
